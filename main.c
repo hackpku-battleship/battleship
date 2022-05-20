64a691d8f6997d945058eee26bdc4b3112e1e410
xzz-233 <xuzhouzi@qq.com>
2022-05-20 14:13:25 +0800

    Initialization

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "raylib.h"
+#include "bullet.h"
+
+int main(void){
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - custom frame control");
+    while (!WindowShouldClose()){
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            
+
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
