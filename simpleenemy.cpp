19aa853481bfd08a7929ac6b935043d5eaa5b3ea
Yuchen Song <1726016246@qq.com>
2022-05-20 18:34:17 +0800

    simple enemy

--- simpleenemy.cpp
+++ simpleenemy.cpp
@@ -6,7 +6,7 @@ SimpleEnemy::SimpleEnemy(float _hp, float _gentime, float _x, float _y, float _r
 std::vector<Bullet> SimpleEnemy::getBullet(float nowTime) {
     float dtime = nowTime - gentime;
     std::vector<Bullet> ret;
-    if (dutime <= 0 && dtime >= 0.5) {
+    if (dutime <= 0.5 && dtime >= 0.5) {
         ret.push_back((basicBullet){nowTime, (Vector2){x, y}, (Vector2){0, 1}});
     }
     dutime = dtime;
