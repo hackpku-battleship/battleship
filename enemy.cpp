c109c2bdd433717301380488f2ff9f86cc3ef60f
Yuchen Song <1726016246@qq.com>
2022-05-20 18:46:11 +0800

    fix simple enemy

--- enemy.cpp
+++ enemy.cpp
@@ -4,8 +4,8 @@ Enemy::Enemy(float _hp, float _gentime, float _x, float _y, float _r) :
     hp(_hp), gentime(_gentime), x(_x), y(_y), r(_r), dutime(0.0){
 }
 
-std::vector<Bullet> Enemy::getBullet(float nowtime) {
-    return std::vector<Bullet>();
+std::vector<Bullet*> Enemy::getBullet(float nowtime) {
+    return std::vector<Bullet*>();
 }
 
 float Enemy::getX() {
