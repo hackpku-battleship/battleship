3d6feb79ffaaca30c4c27ef6187c3c9ffbe23992
Yuchen Song <1726016246@qq.com>
2022-05-20 20:40:28 +0800

    d enemy

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(void)
     freopen("log", "w", stderr);
     const int screenWidth = 800;
     const int screenHeight = 600;
-    Player player({400, 600}, 10, 5, 50, 300);
+    Player player({400, 600}, 10, 5, 5, 300);
     PlayerHPBar playerHPBar(10, screenHeight - 20, 10, 25);
     InitWindow(screenWidth, screenHeight, "raylib [core] example - custom frame control");
     SetTargetFPS(60);
@@ -25,6 +25,7 @@ int main(void)
 
         if (flag == 0) {
             enemys.addEnemy(new SimpleEnemy(100.0, time, 20, 400, 50, 5));
+            //std::cerr << "ADD E" << std::endl;
             flag = 1;
         }
 
@@ -36,6 +37,11 @@ int main(void)
         if (IsKeyDown(KEY_X))
         {
             playerBullets.addBullet(new basicBullet(time, 5, player.getPosition(), {(1. * rand() / RAND_MAX - 0.5) * 100, (1. * rand() / RAND_MAX - 0.5) * 100}));
+        }
+
+        if (IsKeyPressed(KEY_U)) {
+            float y = 100 + (1.0 * rand() / RAND_MAX - 0.5) * 2.0 * 80;
+            enemys.addEnemy(new DEnemy(100.0, time, 10 , 1.0 * rand() / RAND_MAX * screenWidth, y, 6.0));
         }
         
         auto bullets = enemys.updateTime(time);
